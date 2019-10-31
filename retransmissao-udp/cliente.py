f3612845f9fbd3a249be9abce092c9e383bca2eb
Jadson Luan <jadsonluan1@gmail.com>
2019-10-31 19:35:53 -0300

    Encapsula RDT em um modulo

--- retransmissao-udp/cliente.py
+++ retransmissao-udp/cliente.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-import socket
+import socket, rdt
 
 HOST = '127.0.0.1'     # Endereco IP do Servidor
 PORT = 5000            # Porta que o Servidor esta
@@ -8,36 +8,11 @@ dest = (HOST, PORT)
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-TENTATIVAS_MAXIMAS = 3  # Numero de tentativas de reenvio por timeout
-INTERVALO_TIMEOUT = 2   # Em segundos
-
-def envia_msg(msg, dest, tentativas):
-  if tentativas <= 0: 
-    udp.settimeout(None)
-    print "Não foi possível enviar a mensagem. Número máximo de timeouts atingidos."
-    return
-
-  try:
-    udp.sendto(msg, dest)
-    udp.settimeout(2)
-    msg, cliente = udp.recvfrom(1024)
-    while not (cliente == dest and msg == 'ACK'): msg, cliente = udp.recvfrom(1024)    
-  except socket.timeout:
-    print "Timeout! Tentando reenviar... Tentativas restates:", (tentativas - 1)
-    envia_msg(msg, dest, tentativas - 1)
-    
-  udp.settimeout(None)
-
-def recebe_msg():
-  msg, cliente = udp.recvfrom(1024)
-  udp.sendto('ACK', cliente)
-  return msg, cliente
-
 print 'Para sair use CTRL+X\n'
-msg = ''
+msg = raw_input('Digite a mensagem: ')
 while msg <> '\x18':
-  msg = raw_input('Digite a mensagem: ')
-  envia_msg(msg, dest, TENTATIVAS_MAXIMAS)
-  resposta, cliente = recebe_msg()
+  rdt.send(udp, msg, dest)
+  resposta, cliente = rdt.recv(udp)
   print "Recebido de volta:", resposta
+  msg = raw_input('Digite a mensagem: ')
 udp.close()
---
+++ retransmissao-udp/rdt.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+import socket
+
+__name__ = "rdt"
+
+MAX_ATTEMPTS = 3  # Numero de tentativas de reenvio por timeout
+TIMEOUT_INTERVAL = 2   # Em segundos
+
+def send(udp, msg, dest):
+  send_recursive(udp, msg, dest, MAX_ATTEMPTS)
+
+def send_recursive(udp, msg, dest, attempts):
+  if attempts <= 0:
+    udp.settimeout(None)
+    print "Não foi possível enviar a mensagem. Número máximo de timeouts atingidos."
+    return
+
+  try:
+    udp.sendto(msg, dest)
+    udp.settimeout(TIMEOUT_INTERVAL)
+    msg, sender = udp.recvfrom(1024)
+    while not (sender == dest and msg == 'ACK'): msg, sender = udp.recvfrom(1024)    
+  except socket.timeout:
+    print "Timeout! Tentando reenviar... Tentativas restates:", (attempts - 1)
+    send(udp, msg, dest, attempts - 1)
+
+  udp.settimeout(None)
+
+def recv(udp):
+  msg, sender = udp.recvfrom(1024)
+  udp.sendto('ACK', sender)
+  return msg, sender
